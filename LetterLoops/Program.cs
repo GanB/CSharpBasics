2ffc44d31a33b67c1ea682e104f80868557b6bef
Ganesh Babu <ganeshbabudev65@gmail.com>
2023-01-30 20:22:13 -0500

    Add project files.

---
+++ LetterLoops/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace LetterLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Letter Loops");
+            Console.WriteLine("Please enter a string:");
+
+            var sb = new StringBuilder();
+            bool isStringValid = false;
+            string input;
+
+            do
+            {
+                input = Console.ReadLine();
+                isStringValid = !string.IsNullOrEmpty(input);
+
+                if (!isStringValid)
+                {
+                    Console.WriteLine("Please enter a valid string:");
+                }
+                else
+                {
+                    sb.Append(char.ToUpper(input[0]));
+
+                    for (int i = 1; i < input.Length; i++)
+                    {
+                        sb.Append("-");
+                        sb.Append(char.ToUpper(input[i]));
+                        sb.Append(new String(char.ToLower(input[i]), i));
+
+                    }
+                    Console.WriteLine(sb);
+                }
+            } while (!isStringValid);
+        }
+    }
+}
